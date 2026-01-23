9ef2cdccc495664f2e8e9648f2057c6040cad62b
Puneet Kumar <kpuneetoff@gmail.com>
2026-01-23 09:10:42 +0530

    Bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
-import User from "../models/user.model.js";
+import {User} from "../models/user.model.js";
 import uploadOnCLoudinary from "../utils/fileupload.js";
 import ApiResponse from "../utils/ApiResponse.js";
 
@@ -26,7 +26,7 @@ const regUser = asyncHandler(async (req, res) => {
   ) {
     throw new ApiError(400, "Field is required");
   }
-  const existedUser = User.findOne({
+  const existedUser =await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -35,7 +35,10 @@ const regUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath= req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage)&& req.files.coverImage.length>0){
+    coverImageLocalPath=req.files.coverImage[0].path
+  }
 
   if(!avatarLocalPath){
     throw new ApiError(400,"Avatar File is required")
